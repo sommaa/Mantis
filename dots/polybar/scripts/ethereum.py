2021cfae6de79ec69d8cc2c818179a9fe9f61603
Andrea Somma <120776791+sommaa@users.noreply.github.com>
2023-02-27 20:26:53 +0100

    Create ethereum.py

---
+++ dots/polybar/scripts/ethereum.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import requests
+from decimal import Decimal
+
+# Everything except the general section
+currency = 'ethereum'
+base_currency = 'USD'
+params = {'convert': base_currency}
+
+try:
+    json_store = requests.get(f'https://api.coingecko.com/api/v3/coins/{currency}').json()["market_data"]
+    local_price = round(Decimal(json_store["current_price"][f'{base_currency.lower()}']), 2)
+    sys.stdout.write(f'{local_price}'+'$'+'  ')
+ 
+    with open("/home/andrea/.cache/ethereum_cache.json", "w") as outfile:
+        json.dump(json_store, outfile)
+
+    
+except requests.exceptions.ConnectionError as e:
+    # Opening JSON file
+    with open('/home/andrea/.cache/ethereum_cache.json', 'r') as openfile:
+        # Reading from json file
+        json_store = json.load(openfile)
+    
+    local_price = round(Decimal(json_store["current_price"][f'{base_currency.lower()}']), 2)
+    sys.stdout.write(f'{local_price}'+'$'+'  ')
